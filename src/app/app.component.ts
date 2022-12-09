9acf4a17ffbf1a274f38ecdf7686722343bed743
jasonlui <cherngen6@gmail.com>
2022-12-10 03:04:53 +0800

    1210

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { LoginService } from './core/service/api/login.service';
 import { ShowLoginService } from './core/service/show-login.service';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -8,24 +8,38 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angularGooleLogin';
   con!: number;
-
+  show = '';
   constructor(
     private showLoginService: ShowLoginService,
     private loginService: LoginService,
     private http: HttpClient
   ) {}
+  ngOnInit(): void {
+    const s = window.location.search;
+    if (s) {
+      let a = s.split('?');
+      let b =decodeURIComponent(a[1]).split('&');
+      b.forEach((x, i) => {
+        console.log('b', b[i]);
+        this.show += b[i].split('=')+'<br>';
+
+      });
+    }
+  }
   showlist() {
+    window.location.href = 'http://localhost:3000/auth/google';
+
     // const googlelog = this.loginService.googleLogin();
     // this.loginService.googleLogin().subscribe((data) => {
     //   console.log('data', data);
     // });
-    console.log('in')
-    this.http.get('http://localhost:4200/auth/google').subscribe((data) => {
-      console.log(data);
-    });
+    // console.log('in')
+    // this.http.get('http://localhost:4200/auth/google').subscribe((data) => {
+    //   console.log(data);
+    // });
     // console.log('tt', tempgoogle);
     // const tempgoogle = this.showLoginService.showlog().subscribe((data) => {
     //   console.log('data', data);
